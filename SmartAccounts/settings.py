3c278f6106216efe49a72b7c5b3bf5e6080f8fa1
Dimplektech <dimplekaware@gmail.com>
2025-07-13 10:55:49 +0100

    fix: Update ALLOWED_HOSTS to allow all hosts for development

--- SmartAccounts/settings.py
+++ SmartAccounts/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-temp-key-for-build")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
